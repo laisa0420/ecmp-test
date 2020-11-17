856b8a7667b90577f3113a42334c5c385a08b214
laisa_crv <laisa0420@gmail.com>
2020-11-17 22:45:02 +0000

    maximum of 100 random numbers in verilog

--- circuit.v
+++ circuit.v
@@ -2,16 +2,31 @@
 `include "iob_lib.vh"
 
 module circuit
+  #(
+  	parameter W=32
+   )
+  
   (
    `INPUT(rst,1),
    `INPUT(clk,1),
    `INPUT(en,1),
-   `INPUT(x,32),
-   `OUTPUT(y,32)
+   `INPUT(X,W),
+   `OUTPUT(Y,W)
    );
 
-   `SIGNAL(acc, 32)
-   `ACC_ARE(clk, rst, 1'b0, en, acc, x)
-   `SIGNAL2OUT(y, acc)
+   `SIGNAL(new_max, W)
+   `SIGNAL(max, W)
+   `SIGNAL2OUT(Y, new_max)
+   `SIGNAL(cnt,7)
+   
+   `REG_RE(clk,rst,1'b0,en & cnt!=99,max,new_max)
+   
+   `COUNTER_RE(clk, rst, cnt!=99, cnt)
+   
+   `COMB begin
+   	if (X>max) new_max=X;
+    end
+   
+   
    
 endmodule
--- circuit_tb.v
+++ circuit_tb.v
@@ -1,36 +1,44 @@
 `timescale 1ns / 1ps
 `include "iob_lib.vh"
 
+`define W 96
+
 module circuit_tb;
 
    `CLOCK(clk, 10)
    `RESET(rst, 1, 10)
    `SIGNAL(en, 1)
-   `SIGNAL(x, 32)
-   `SIGNAL_OUT(y, 32)
+   `SIGNAL(x,`W)
+   `SIGNAL_OUT(y, `W)
+   
 
-   integer i;
+   integer i,seed;
    
    initial begin
+      seed=7;
       $dumpfile("circuit.vcd");
       $dumpvars();
       en=0;
       for (i=0; i<100; i=i+1) begin
-         @(posedge clk) #1 x=i; en=1;
-         @(posedge clk) #1 en=0;
+      	x=$urandom(seed);
+        @(posedge clk) #1 en=1;$display("%d %d",x,y);
       end
 
       @(posedge clk) #100 $finish;
 
    end 
 
-   circuit c0
+   circuit
+     #(
+       .W(`W)
+       ) 
+   c0
      (
       .rst(rst),
       .clk(clk),
       .en(en),
-      .x(x),
-      .y(y)
+      .X(x),
+      .Y(y)
       );
 
 
